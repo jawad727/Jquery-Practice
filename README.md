903d05e5d17ec6c35df6de8a103ceaa48032f468
jawad727 <jawaddhussein@gmail.com>
2020-03-12 13:54:53 -0700

    event binding NOT DRY

--- README.md
+++ README.md
@@ -2,7 +2,9 @@
 A repo where I go back and practice my Jquery skills
 
 - $ is an alias for a jquery object 
+
 - There are 2 parts to every jquery action, the selector and the method 
-    - Ex: $("#panel1).hide(300);
+    - Ex: $("#panel1").hide(300);
+
 - You can also chain methods
-    - Ex: $("#panel1).hide(300).show(1000);
+    - Ex: $("#panel1").hide(300).show(1000);
--- index.js
+++ index.js
@@ -1,11 +1,40 @@
 // console.log("asdasd")
 
-$(function() {
+$(function() { // These are all basic Jquery commands
 
     // $(".num1").hide(300).show(1000)
 
     // $(".num1").slideUp(1000).delay(1000).slideDown(1000)
 
-    $(".num1").fadeOut(1000).delay(1000).fadeIn(1000)
+    // $(".num1").fadeOut(1000).delay(1000).fadeIn(1000)
+
+    // $(".num1").css({
+    //     color: "red", 
+    //     fontWeight: "bold"
+    // })
+
+    // $(".panel1 p").html("text changed!")
 
 })
+
+$(function() { // Event Binding (NOT DRY)
+
+    // $("#btn1").on("click", function() {
+    //     $(".panel1").slideToggle(200);
+    // });
+
+    // $("#btn2").on("click", function() {
+    //     $(".panel2").slideToggle(200);
+    // });
+
+    // $("#btn3").on("click", function() {
+    //     $(".panel3").slideToggle(200);
+    // });
+
+    // $("#btn4").on("click", function() {
+    //     $(".panel4").slideToggle(200);
+    // });
+
+
+})
+
